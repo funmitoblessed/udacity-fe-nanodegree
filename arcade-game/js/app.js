8d9a89adf676b9274b890f1f9d3a95cddcf834c1
Funmito <funmito.odefemi@outlook.com>
2018-09-18 15:49:32 +0100

    Instantiate new player and enemy objects

--- arcade-game/js/app.js
+++ arcade-game/js/app.js
@@ -3,6 +3,7 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,12 +25,46 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // image sprite for default player
+    this.sprite = 'images/char-pink-girl.png';
+
+};
+
+// update method for the Player class
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// render method for the Player class
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// handle input method for the Player class
+Player.prototype.handleInput = function() {
+
+
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+let player = new Player();
+
+let enemyOne = new Enemy();
+let enemyTwo = new Enemy();
+let enemyThree = new Enemy();
+let enemyFour = new Enemy();
+
+let allEnemies = [enemyOne, enemyTwo, enemyThree, enemyFour];
+
+
+console.log(allEnemies, player);
 
 
 // This listens for key presses and sends the keys to your
